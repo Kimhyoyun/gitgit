447eb2aa6be50532e633511f4db1bdd3b836f2f1
KHY <khyun1380@opensg.net>
2024-07-18 18:12:08 +0900

    -test

---
+++ InterfaceExample/src/main/java/com/example/InterfaceExample/InterfaceExampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.InterfaceExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceExampleApplication.class, args);
+		
+		
+	}
+
+}
